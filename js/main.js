523a748d8029ba14ad28197a0f27ba025983563a
RapidSigma <103945760+RapidSigma@users.noreply.github.com>
2023-04-20 21:42:48 +0530

    Changed Gallery Design

--- js/main.js
+++ js/main.js
@@ -12,71 +12,7 @@ $(document).click(function (event) {
     }
 });
 
-//Next Carousel
-const nextCarousel = () => {
-    if(carsouselImages[carsouselImages.length - 1]) {
-      carousel.scrollTo(0, 0); 
-    } 
-      carousel.scrollBy(300, 0);
-};
 
-nextBtn.addEventListener('click', e => {
-  nextCarousel(); 
-});
-
-//Prev Carousel
-const prevCarousel = () => {
-   if(carsouselImages[0]) {
-    carousel.scrollTo(4800,0);
-   }
-    carousel.scrollBy(-300, 0); 
-};
-
-prevBtn.addEventListener('click', e => {
-   prevCarousel(); 
-});
-
-
-// Auto carousel
-const auto = true; // Auto scroll
-const intervalTime = 5000;
-let sliderInterval;
-
-
-if (auto) {
-  sliderInterval = setInterval(nextCarousel, intervalTime);
-};
-
-carousel.addEventListener('mouseover', (stopInterval) => {
-  clearInterval(sliderInterval);
-});
-
-carousel.addEventListener('mouseleave', (startInterval) => {
-  if (auto) {
-    sliderInterval = setInterval(nextCarousel, intervalTime);
-  }
-}); 
-
-//for mobile events
-carousel.addEventListener('touchstart', (stopIntervalT) => {
-    clearInterval(sliderInterval);
-});
-carousel.addEventListener('touchend', (startIntervalT) => {
-   if (auto) {
-    sliderInterval = setInterval(nextCarousel, intervalTime);
-  }
-});
-
-//Debounce
-var previousCall;
-window.addEventListener('resize', () => {
-    if (previousCall >= 0) {
-        clearTimeout(previousCall);
-    } 
-    previousCall = setTimeout(() => {
-      carousel.scrollBy(-300, 0); 
-    }, 200);
-}); 
 
 (function ($) {
     "use strict";
@@ -154,7 +90,28 @@ window.addEventListener('resize', () => {
     });
 
 
-    
+    // gallery carousel
+    $(".gallery-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        center: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            }
+        }
+    });
     // themes carousel
     $(".theme-carousel").owlCarousel({
         autoplay: true,
